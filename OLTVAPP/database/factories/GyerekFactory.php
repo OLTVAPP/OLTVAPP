5b635f603fc238cfea56c981f02887f0fe8d8a10
Martonnvok <martonsom@gmail.com>
2024-01-30 12:11:52 +0100

    Factoryk feltöltése (10 a 14-ből)

--- OLTVAPP/database/factories/GyerekFactory.php
+++ OLTVAPP/database/factories/GyerekFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Orvos;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Gyerek>
@@ -17,7 +19,15 @@ class GyerekFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'orvos_id' => Orvos::all()->random()->felhasznalo_id,
+            'vez_nev' => $this->faker->name(),
+            'ker_nev' => $this->faker->name(),
+            'lakcim_varos' => Str::random(10),
+            'lakcim_irSzam' => $this->faker->randomNumber(1),
+            'lakcim_utca' => $this->faker->randomNumber(1),
+            'erzekenyseg' => Str::random(10),
+            'szul_datum' => $this->faker->date(),
+            'szul_hely' => Str::random(10),
         ];
     }
 }
